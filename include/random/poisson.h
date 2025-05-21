8e0840b49022d1040854060da23bfa32ad656fe0
tangxiangong <tangxiangong@gmail.com>
2025-05-21 09:53:43 +0800

    feat: add Poisson class and random Poisson number generation
    - Introduced Poisson class for generating random samples from a Poisson distribution.
    - Implemented rand_poisson function to handle random Poisson number generation with error checking for the rate parameter.
    - Enhanced error handling with informative messages for invalid rate values.

---
+++ include/random/poisson.h
@@ -0,0 +1,54 @@
+#ifndef POISSON_H
+#define POISSON_H
+
+#include <vector>
+#include <format>
+#include <random>
+#include "../error.h"
+#include "random/utils.h"
+
+using std::vector;
+using std::format;
+
+template<typename T = unsigned int>
+Result<vector<T> > rand_poisson(size_t n, double rate = 1.0) {
+    if (rate <= 0) {
+        return Err(Error::InvalidArgument(format(
+            "The rate `rate` must be positive, but got {}",
+            rate
+        )));
+    }
+    auto sampler = [rate]() mutable {
+        thread_local static std::mt19937 gen = generator();
+        std::poisson_distribution<T> dist(rate);
+        return dist(gen);
+    };
+    return Ok(parallel_generate<T>(n, sampler));
+}
+
+class Poisson {
+    double m_rate = 1.0;
+
+public:
+    Poisson() = default;
+
+    explicit Poisson(double rate) : m_rate(rate) {
+        if (m_rate <= 0) {
+            throw std::invalid_argument(format(
+                "The rate parameter `rate` must be positive, but got {}",
+                m_rate
+            ));
+        }
+    }
+
+    [[nodiscard]] double get_rate() const {
+        return m_rate;
+    }
+
+    template<typename T = unsigned int>
+    [[nodiscard]] Result<vector<T> > sample(size_t n) const {
+        return rand_poisson<T>(n, m_rate);
+    }
+};
+
+#endif //POISSON_H
