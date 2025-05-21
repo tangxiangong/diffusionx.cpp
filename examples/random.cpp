021f60ef7b7c775f1badf880d44ebd04af6b01bb
tangxiangong <tangxiangong@gmail.com>
2025-05-21 09:53:58 +0800

    docs: enhance random number generation example with Exponential and Poisson distributions
    - Added functionality to generate random samples from Exponential and Poisson distributions in the random.cpp example.
    - Implemented error handling and output for generated values from randexp and rand_poisson functions.
    - Maintained timing logic for performance measurement of random number generation.

--- examples/random.cpp
+++ examples/random.cpp
@@ -1,22 +1,68 @@
 #include <print>
 #include "random/normal.h"
+#include "random/exponential.h"
 #include <chrono>
 
+#include "random/poisson.h"
+
 int main() {
-    
-    auto start_time = std::chrono::high_resolution_clock::now();
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
 
-    auto result = randn(1000000000);
+        auto normal = randn(3);
 
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
-    if (!result) {
-        std::println("error: {}", result.error().message);
-        return 1;
+        if (!normal) {
+            std::println("error: {}", normal.error().message);
+            return 1;
+        }
+        for (const auto &val: normal.value()) {
+            std::println("{}", val);
+        }
+
+        // std::println("Time taken by randn: {} ms", duration.count());
     }
 
-    std::println("Time taken by rands: {} ms", duration.count());
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
 
+        auto exp = randexp(3);
+
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+        if (!exp) {
+            std::println("error: {}", exp.error().message);
+            return 1;
+        }
+
+        for (const auto &val: exp.value()) {
+            std::println("{}", val);
+        }
+
+        // std::println("Time taken by randexp: {} ms", duration.count());
+    }
+
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+
+        auto poisson = rand_poisson<unsigned long>(3);
+
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+        if (!poisson) {
+            std::println("error: {}", poisson.error().message);
+            return 1;
+        }
+
+        for (const auto &val: poisson.value()) {
+            std::println("{}", val);
+        }
+
+        // std::println("Time taken by randexp: {} ms", duration.count());
+    }
     return 0;
 }
