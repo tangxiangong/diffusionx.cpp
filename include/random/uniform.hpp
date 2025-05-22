7927532d2f25fbf268e547c958f16e9f6b83797c
tangxiangong <tangxiangong@gmail.com>
2025-05-22 09:52:51 +0800

    feat: add overloaded rand function for uniform random number generation
    - Introduced an overloaded version of the `rand` function to generate uniform random numbers with specified bounds.
    - Enhanced error handling for invalid bounds to ensure proper input.
    - Utilized thread-local random number generation for improved performance and consistency.

--- include/random/uniform.hpp
+++ include/random/uniform.hpp
@@ -3,14 +3,14 @@
 #include <vector>
 #include <format>
 #include <type_traits>
-#include "../error.h"
+#include "../error.hpp"
 #include "random/utils.hpp"
 
 using std::vector;
 using std::format;
 
 template<typename T = double> requires std::is_floating_point_v<T> || std::is_integral_v<T>
-Result<vector<T> > rand(size_t n = 1, T a = 0, T b = 1) {
+Result<vector<T> > rand(size_t n, T a = 0, T b = 1) {
     if (a > b) {
         return Err(Error::InvalidArgument(format(
             "The lower bound `a` must be less than the upper bound `b`, but got {} > {}",
@@ -18,7 +18,7 @@ Result<vector<T> > rand(size_t n = 1, T a = 0, T b = 1) {
         )));
     }
 
-    if (std::is_integral_v<T>) {
+    if constexpr (std::is_integral_v<T>) {
         auto sampler = [a, b]() mutable {
             thread_local std::mt19937 gen = generator();
             std::uniform_int_distribution<T> dist{a, b};
@@ -34,5 +34,26 @@ Result<vector<T> > rand(size_t n = 1, T a = 0, T b = 1) {
         return Ok(parallel_generate<T>(n, sampler));
     }
 }
+
+template<typename T = double> requires std::is_floating_point_v<T> || std::is_integral_v<T>
+Result<T> rand(T a = 0, T b = 1) {
+    if (a > b) {
+        return Err(Error::InvalidArgument(format(
+            "The lower bound `a` must be less than the upper bound `b`, but got {} > {}",
+            a, b
+        )));
+    }
+
+    if constexpr (std::is_integral_v<T>) {
+        thread_local std::mt19937 gen = generator();
+        std::uniform_int_distribution<T> dist{a, b};
+        return dist(gen);
+    } else {
+        thread_local std::mt19937 gen = generator();
+        std::uniform_real_distribution<T> dist{a, b};
+        return dist(gen);
+    }
+}
+
 
 #endif //UNIFORM_H
