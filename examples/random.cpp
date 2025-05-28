b34a4192ce067de4c7cb9c4224f5b25949a40f82
tangxiangong <tangxiangong@gmail.com>
2025-05-28 08:42:40 +0800

    refactor(docs): simplify random.cpp structure and improve readability
    - Removed unnecessary block scopes to enhance code clarity.
    - Streamlined error handling for random number generation functions.
    - Improved overall organization of the main function for better readability.

--- examples/random.cpp
+++ examples/random.cpp
@@ -1,29 +1,23 @@
 #include <print>
-#include <chrono>
 
 import diffusionx.random;
 
 int main() {
-    size_t len = 3;
-    {
+    size_t len = 3; {
         auto normal = randn<float>(len);
         if (!normal) {
             std::println("error: {}", normal.error().message);
             return 1;
         }
         std::println("randn: {}", normal.value());
-    }
-
-    {
+    } {
         auto exp = randexp<float>(len);
         if (!exp) {
             std::println("error: {}", exp.error().message);
             return 1;
         }
         std::println("randexp: {}", exp.value());
-    }
-
-    {
+    } {
         auto poisson = rand_poisson<unsigned long>(len);
         if (!poisson) {
             std::println("error: {}", poisson.error().message);
